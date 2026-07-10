6002ed02995c054827fccf7ef7c3d076737ecd6e
ck0xdev <kukadiyachintan026@gmail.com>
2026-07-10 10:24:22 +0530

    feat: add 'REST APIs' skill tag back to About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,6 +23,7 @@ const About = () => {
           <div className="skill-tag">Git & GitHub</div>
           <div className="skill-tag">UI/UX Design</div>
           <div className="skill-tag">Figma</div>
+          <div className="skill-tag">REST APIs</div>
         </div>
       </div>
     </section>
